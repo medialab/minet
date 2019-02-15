e242e9c7e1aa89763c37d69b0c4add4e705c3417
farjasju <julesfarjas@gmail.com>
2019-02-15 22:27:23 +0100

    Solving progress bar stdout conflict

--- minet/cli/facebook_action.py
+++ minet/cli/facebook_action.py
@@ -7,6 +7,7 @@
 
 import os
 import csv
+import sys
 from minet.cli.utils import custom_reader
 from minet.facebook import fetch_share_count
 
@@ -15,18 +16,25 @@ from progress.bar import FillingSquaresBar
 
 def facebook_action(namespace):
 
-    bar = FillingSquaresBar('Fetching FB shares')
-
     headers, position, reader = custom_reader(namespace.file, namespace.column)
 
     headers.append("facebook_share_count")
     writer = csv.writer(namespace.output)
     writer.writerow(headers)
 
-    for line in reader:
-        bar.next()
-        url = line[position]
-        line.append(fetch_share_count(url))
-        writer.writerow(line)
-    bar.goto(100)
-    bar.finish()
+    if namespace.output == sys.stdout:
+        for line in reader:
+            url = line[position]
+            line.append(fetch_share_count(url))
+            writer.writerow(line)
+
+    else:
+        bar = FillingSquaresBar('Fetching FB shares')
+
+        for line in reader:
+            bar.next()
+            url = line[position]
+            line.append(fetch_share_count(url))
+            writer.writerow(line)
+        bar.goto(100)
+        bar.finish()
