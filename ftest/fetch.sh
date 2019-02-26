b4c2134e8b3b3b09d8498790cafe40ff208b5f10
Yomguithereal <guillaumeplique@gmail.com>
2019-02-26 17:46:14 +0100

    Commenting shell file

--- ftest/fetch.sh
+++ ftest/fetch.sh
@@ -1,5 +1,8 @@
+# Cleanup
 rm -rf ftest/content
 rm -f ftest/report.csv
+
+# Fetching url from csv file
 python -m minet.cli fetch url ftest/resources/urls.csv \
   -d ftest/content \
   --total 10000 \
