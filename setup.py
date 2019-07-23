adf090145f82ca1e82f27e76e6aaf0f60c19cc22
Yomguithereal <guillaumeplique@gmail.com>
2019-07-23 17:03:54 +0200

    Bump 0.7.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('./README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='minet',
-      version='0.6.0',
+      version='0.7.0',
       description='A webmining CLI tool & library for python.',
       long_description=long_description,
       long_description_content_type='text/markdown',
