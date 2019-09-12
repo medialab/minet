aeb8cecabd936cbf874d52b321cff5aa3d43845a
Yomguithereal <guillaumeplique@gmail.com>
2019-09-12 16:53:05 +0200

    Upgrading ural

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(name='minet',
         'numpy>=1.16.1',
         'quenouille>=0.3.0',
         'tqdm>=4.31.1',
-        'ural>=0.7.0',
+        'ural>=0.9.0',
         'urllib3[secure]>=1.25.3',
         # 'dragnet>=2.0.3'
       ],
