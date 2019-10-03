e3f0954a2c88a611839ffbe08a41ef8087ef2027
Yomguithereal <guillaumeplique@gmail.com>
2019-10-03 14:31:37 +0200

    More ftest

--- ftest/resolve.py
+++ ftest/resolve.py
@@ -28,26 +28,3 @@ for url in URLS:
     print(error)
     for item in stack:
         print(item)
-
-# import csv
-# from minet import multithreaded_resolve
-# from tqdm import tqdm
-
-# with open('./ftest/resources/resolutions.csv') as f, \
-#      open('./ftest/resolved.csv', 'w') as of:
-#     reader = csv.DictReader(f)
-#     writer = csv.writer(of)
-#     writer.writerow(['original', 'resolved', 'error'])
-
-#     for result in tqdm(multithreaded_resolve(reader, key=lambda x: x['url'], threads=100), total=10000):
-#         stack = result.stack
-#         error = result.error
-
-#         original = stack[0][1] if stack else ''
-#         last = stack[-1][1] if stack else ''
-
-#         writer.writerow([
-#             original,
-#             last if last != original else '',
-#             str(error) if error else ''
-#         ])
