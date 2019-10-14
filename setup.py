5a3d9a364794790d0c1a2bf3487b49b7b85e5981
Yomguithereal <guillaumeplique@gmail.com>
2019-10-14 18:08:52 +0200

    Bump 0.18.0

--- setup.py
+++ setup.py
@@ -34,8 +34,7 @@ setup(name='minet',
         'quenouille>=0.4.0',
         'tqdm>=4.31.1',
         'ural>=0.11.0',
-        'urllib3[secure]>=1.25.3',
-        # 'dragnet>=2.0.4'
+        'urllib3[secure]>=1.25.3'
       ],
       entry_points={
         'console_scripts': ['minet=minet.cli.__main__:main']
