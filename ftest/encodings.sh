be2ffc23283cf5f9a5f2bb58ebc768735900e394
Yomguithereal <guillaumeplique@gmail.com>
2019-10-14 14:15:28 +0200

    Retrieving encodings

---
+++ ftest/encodings.sh
@@ -0,0 +1,4 @@
+URL=https://docs.python.org/3.4/library/codecs.html#standard-encodings
+
+python -m minet.cli fetch $URL | \
+python -m minet.cli scrape ftest/scrapers/encodings.yml > ftest/encodings.csv
