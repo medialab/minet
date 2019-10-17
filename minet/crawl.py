d6e7983ea819d9dafd8e91863cd9f77914b2a1f7
Yomguithereal <guillaumeplique@gmail.com>
2019-10-17 15:36:19 +0200

    Notes

--- minet/crawl.py
+++ minet/crawl.py
@@ -152,6 +152,10 @@ class Spider(object):
 def crawl(spec, queue_path=None, threads=25, buffer_size=DEFAULT_GROUP_BUFFER_SIZE,
           throttle=DEFAULT_THROTTLE):
 
+    # NOTE: crawling could work depth-first but:
+    #   1. LifoQueue has no persistent equivalent yet
+    #   2. buffer_size should be 0 (requires to fix quenouille issue #1)
+
     # Memory queue
     if queue_path is None:
         queue = Queue()
