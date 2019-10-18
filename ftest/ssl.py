7ae601313987af49a70aa150b6325142b21f1ad2
Yomguithereal <guillaumeplique@gmail.com>
2019-10-18 11:40:49 +0200

    Adding ftest.ssl. Related to #78

---
+++ ftest/ssl.py
@@ -0,0 +1,25 @@
+from minet.utils import create_pool, raw_resolve
+
+SSL_ISSUES = [
+    'https://lemde.fr/2zmunsV',
+    'https://buff.ly/2Nnaevg',
+    'http://www.plateforme1418.com/',
+    'https://www.silverday-normandie.fr',
+    'http://swll.to/rJjizGY',
+    'http://ow.ly/zpnt30mdb9N'
+]
+
+http = create_pool()
+
+for url in SSL_ISSUES:
+    print(url)
+    err, stack, response = raw_resolve(http, url, return_response=True)
+    print('Error', err, type(err))
+
+    if response is not None:
+        print(response)
+
+    for r in stack:
+        print(r)
+
+    print()
