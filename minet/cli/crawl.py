67dd05f4bf2b12eac1297d3808638197deca8cb9
Yomguithereal <guillaumeplique@gmail.com>
2019-10-30 14:02:52 +0100

    Crawling CLI

--- minet/cli/crawl.py
+++ minet/cli/crawl.py
@@ -5,6 +5,8 @@
 # Logic of the crawl action.
 #
 import os
+from os.path import join
+from tqdm import tqdm
 
 from minet.crawl import Crawler
 from minet.utils import load_definition
@@ -15,7 +17,16 @@ def crawl_action(namespace):
     # Scaffolding output directory
     os.makedirs(namespace.output_dir, exist_ok=True)
 
+    queue_path = None
+
+    if namespace.persist_queue:
+        queue_path = join(namespace.output_dir, 'queue')
+
     # Loading crawler definition
     definition = load_definition(namespace.crawler)
 
-    crawler = Crawler(definition, throttle=namespace.throttle)
+    crawler = Crawler(
+        definition,
+        throttle=namespace.throttle,
+        queue_path=queue_path
+    )
