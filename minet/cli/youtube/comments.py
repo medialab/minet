30876c027c840a1631c5f4d1881b073f827d4281
paubre <pauline.breteau@outlook.fr>
2020-03-25 18:30:52 +0100

    modifications after pr review

--- minet/cli/youtube/comments.py
+++ minet/cli/youtube/comments.py
@@ -15,6 +15,7 @@ from minet.utils import create_pool, request_json
 URL_TEMPLATE = 'https://www.googleapis.com/youtube/v3/commentThreads?videoId=%(id)s&key=%(key)s&part=snippet,replies&maxResults=100'
 
 CSV_HEADERS = [
+    'video_id',
     'comment_id',
     'author_name',
     'author_channel_url',
@@ -28,42 +29,54 @@ CSV_HEADERS = [
 ]
 
 
-def time_in_seconds():
+def seconds_to_midnight_pacific_time():
     now_utc = timezone('utc').localize(datetime.utcnow())
     pacific_time = now_utc.astimezone(timezone('US/Pacific')).replace(tzinfo=None)
     midnight_pacific = datetime.combine(pacific_time, datetime.min.time())
     return (midnight_pacific - pacific_time).seconds
 
 
-def get_replies(data_replies, data):
+def get_replies(data_replies):
 
+    data = []
     for item in data_replies:
 
         snippet = item['snippet']
+        video_id = ''
+        comment_id = item['id']
+        author_name = snippet['authorDisplayName']
+        author_channel_url = snippet['authorChannelUrl']
+        author_channel_id = snippet['authorChannelId']['value']
+        text = snippet['textOriginal']
+        like_count = snippet['likeCount']
+        publisehd_at = snippet['publishedAt']
+        updated_at = snippet['updatedAt']
+        total_reply = 0
+        reply_to = snippet['parentId']
 
-        data.append({
-            'comment_id': item['id'],
-            'author_name': snippet['authorDisplayName'],
-            'author_channel_url': snippet['authorChannelUrl'],
-            'author_channel_id': snippet['authorChannelId']['value'],
-            'text': snippet['textOriginal'],
-            'like_count': snippet['likeCount'],
-            'publisehd_at': snippet['publishedAt'],
-            'updated_at': snippet['updatedAt'],
-            'total_reply': None,
-            'reply_to': snippet['parentId']
-        })
+        data.append([
+            video_id,
+            comment_id,
+            author_name,
+            author_channel_url,
+            author_channel_id,
+            text,
+            like_count,
+            publisehd_at,
+            updated_at,
+            total_reply,
+            reply_to
+        ])
 
     return data
 
 
-def get_data(data_json, data):
+def get_data(data_json):
 
-    nextPage = ''
+    data = []
+    next_page = ''
 
-    for key in data_json.keys():
-        if key == 'nextPageToken':
-            nextPage = data_json['nextPageToken']
+    next_page = data_json.get('nextPageToken', None)
 
     for item in data_json['items']:
 
@@ -74,24 +87,37 @@ def get_data(data_json, data):
 
         if total_reply > 0:
             replies = item['replies']
-            data_replies = get_replies(replies['comments'], data)
+            data += get_replies(replies['comments'])
 
         info = top_comment['snippet']
 
-        data.append({
-            'comment_id': top_comment['id'],
-            'author_name': info['authorDisplayName'],
-            'author_channel_url': info['authorChannelUrl'],
-            'author_channel_id': info['authorChannelId']['value'],
-            'text': info['textOriginal'],
-            'like_count': info['likeCount'],
-            'publisehd_at': info['publishedAt'],
-            'updated_at': info['updatedAt'],
-            'total_reply': total_reply,
-            'reply_to': None
-        })
+        video_id = snippet['videoId']
+        comment_id = top_comment['id']
+        author_name = info['authorDisplayName']
+        author_channel_url = info['authorChannelUrl']
+        author_channel_id = info['authorChannelId']['value']
+        text = info['textOriginal']
+        like_count = info['likeCount']
+        publisehd_at = info['publishedAt']
+        updated_at = info['updatedAt']
+        total_reply = total_reply
+        reply_to = ''
 
-    return nextPage, data
+        data.append([
+            video_id,
+            comment_id,
+            author_name,
+            author_channel_url,
+            author_channel_id,
+            text,
+            like_count,
+            publisehd_at,
+            updated_at,
+            total_reply,
+            reply_to
+        ])
+
+    return next_page, data
 
 
 def comments_action(namespace, output_file):
@@ -107,32 +133,19 @@ def comments_action(namespace, output_file):
         unit=' comments',
     )
 
-    def format_csv_row(comments):
-        row = []
-
-        for key in CSV_HEADERS:
-            row.append(comments[key] or '')
-
-        return row
-
     http = create_pool()
 
     url = URL_TEMPLATE % {'id': namespace.id, 'key': namespace.key}
-    err, response, result = request_json(http, url)
-
-    if err:
-        die(err)
-    elif response.status == 403:
-        time.sleep(time_in_seconds())
-    elif response.status >= 400:
-        die(response.status)
-
+    next_page = True
     all_data = []
-    nextPage, all_data = get_data(result, all_data)
 
-    while nextPage:
-        url_next = url + '&pageToken=' + nextPage
-        err, response, result = request_json(http, url_next)
+    while next_page:
+
+        if next_page is True:
+            err, response, result = request_json(http, url)
+        else:
+            url_next = url + '&pageToken=' + next_page
+            err, response, result = request_json(http, url_next)
 
         if err:
             die(err)
@@ -141,8 +154,9 @@ def comments_action(namespace, output_file):
         elif response.status >= 400:
             die(response.status)
 
-        nextPage, all_data = get_data(result, all_data)
+        next_page, data = get_data(result)
+        all_data += data
 
     for comment in all_data:
         loading_bar.update()
-        writer.writerow(format_csv_row(comment))
+        writer.writerow(comment)
