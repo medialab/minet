fefe4d2dadcd7f973ff53fbaac30e76c2e55c815
Yomguithereal <guillaumeplique@gmail.com>
2020-04-16 18:08:34 +0200

    Exposing RateLimiter at package root

--- minet/__init__.py
+++ minet/__init__.py
@@ -10,3 +10,4 @@ from minet.crawl import (
 )
 from minet.fetch import multithreaded_fetch, multithreaded_resolve
 from minet.scrape import scrape, Scraper
+from minet.utils import RateLimiter, RateLimitedIterator
