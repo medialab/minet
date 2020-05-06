858bec5b3cfbfc09b8456b58bf5d6d9e131627ba
Yomguithereal <guillaumeplique@gmail.com>
2020-05-06 18:05:09 +0200

    Advancing

--- minet/cli/crowdtangle/posts_by_id.py
+++ minet/cli/crowdtangle/posts_by_id.py
@@ -4,12 +4,46 @@
 #
 # Logic of the `ct posts-by-id` action.
 #
+import re
 import casanova
+from tqdm import tqdm
+from ural import ensure_protocol
+from ural.facebook import (
+    is_facebook_url,
+    parse_facebook_url,
+    FacebookPost
+)
 
 from minet.cli.utils import die
 from minet.crowdtangle.constants import CROWDTANGLE_POST_CSV_HEADERS
 from minet.crowdtangle.client import CrowdTangleClient
 from minet.crowdtangle.exceptions import CrowdTangleInvalidTokenError
+
+ID_PATTERN = re.compile(r'^\d{8,}$')
+
+
+def is_post_id(value):
+    return bool(re.search(ID_PATTERN, value))
+
+
+def extract_facebook_post_id(value):
+    value = value.strip()
+
+    if not value:
+        return None
+
+    if is_post_id(value):
+        return value
+
+    value = ensure_protocol(value)
+
+    if is_facebook_url(value):
+        parsed = parse_facebook_url(value)
+
+        if isinstance(parsed, FacebookPost):
+            return parsed.id
+
+    return None
 
 
 def crowdtangle_posts_by_id_action(namespace, output_file):
@@ -32,7 +66,17 @@ def crowdtangle_posts_by_id_action(namespace, output_file):
 
     try:
         for row, post_id in enricher.cells(namespace.column, with_rows=True):
-            print(row, post_id)
+            loading_bar.update()
+
+            post_id = extract_facebook_post_id(post_id)
+
+            if post_id is None:
+                enricher.writerow(row)
+                continue
+
+            post = client.post(post_id, format='csv_row')
+
+            print(post)
             break
 
     except CrowdTangleInvalidTokenError:
