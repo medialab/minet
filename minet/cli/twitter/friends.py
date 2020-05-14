4032d0258f3cf2ebe5f78c2cb072bff47b5c65ee
paubre <pauline.breteau@outlook.fr>
2020-05-14 11:49:41 +0200

    twitter friends command

--- minet/cli/twitter/friends.py
+++ minet/cli/twitter/friends.py
@@ -4,7 +4,44 @@
 #
 # Logic of the `tw friends` action.
 #
+import time
+import sys, json
+from minet.utils import create_pool, request_json
+from minet.cli.utils import CSVEnricher, die
+from minet.twitter.utils import TwitterWrapper
+from twitter import *
 
+REPORT_HEADERS = [
+    'friends_id'
+]
 
 def twitter_friends_action(namespace, output_file):
-    print(namespace)
+
+    TWITTER = {
+        'OAUTH_TOKEN' : namespace.access_token,
+        'OAUTH_SECRET' : namespace.access_token_secret,
+        'KEY' : namespace.api_key,
+        'SECRET' : namespace.api_secret_key
+    }
+
+    wrapper = TwitterWrapper(TWITTER)
+
+    enricher = CSVEnricher(
+        namespace.file,
+        namespace.column,
+        output_file,
+        report_headers=REPORT_HEADERS,
+        select=namespace.select.split(',') if namespace.select else None
+    )
+
+    for row in enricher:
+        user_id = row[enricher.pos]
+        all_ids = []
+
+        result = wrapper.call('friends.ids', args={'user_id': user_id})
+        all_ids = result.get('ids', None)
+
+        for id in all_ids:
+            enricher.write(row, [id])
+
+
--- minet/twitter/utils.py
+++ minet/twitter/utils.py
@@ -4,3 +4,50 @@
 #
 # Miscellaneous utility functions to be used in the twitter packages.
 #
+
+
+import sys, json
+from time import time, sleep
+from datetime import datetime
+from twitter import Twitter, OAuth, OAuth2, TwitterHTTPError
+
+class TwitterWrapper(object):
+
+    MAX_TRYOUTS = 5
+
+    def __init__(self, api_keys):
+        self.oauth = OAuth(api_keys['OAUTH_TOKEN'], api_keys['OAUTH_SECRET'], api_keys['KEY'], api_keys['SECRET'])
+        self.oauth2 = OAuth2(bearer_token=json.loads(Twitter(api_version=None, format="", secure=True, auth=OAuth2(api_keys['KEY'], api_keys['SECRET'])).oauth2.token(grant_type="client_credentials"))['access_token'])
+        self.api = {
+            'user': Twitter(auth=self.oauth),
+            'app': Twitter(auth=self.oauth2)
+        }
+        self.waits = {}
+        self.auth = {}
+
+    def call(self, route, args={}, tryouts=MAX_TRYOUTS):
+        if route not in self.auth:
+            self.auth[route] = "user"
+        auth = self.auth[route]
+        try:
+            return self.api[auth].__getattr__("/".join(route.split('.')))(**args)
+        except TwitterHTTPError as e:
+            if e.e.code == 429:
+                now = time()
+                reset = int(e.e.headers["x-rate-limit-reset"])
+                if route not in self.waits:
+                    self.waits[route] = {"user": now, "app": now}
+                self.waits[route][auth] = reset
+                print("REACHED API LIMITS on %s %s until %s for auth %s" % (route, args, reset, auth), file=sys.stderr)
+                minwait = sorted([(a, w) for a, w in self.waits[route].items()], key=lambda x: x[1])[0]
+                if minwait[1] > now:
+                    sleeptime = 5 + max(0, int(minwait[1] - now))
+                    print("  will wait for %s for the next %ss (%s)" % (minwait[0], sleeptime, datetime.fromtimestamp(now + sleeptime).isoformat()[11:19]), file=sys.stderr)
+                    sleep(sleeptime)
+                self.auth[route] = minwait[0]
+                return self.call(route, args, tryouts)
+            elif tryouts:
+                return self.call(route, args, tryouts-1)
+            else:
+                print("ERROR after %s tryouts for %s %s %s" % (self.MAX_TRYOUTS, route, auth, args), file=sys.stderr)
+                print(sys.stderr, "%s: %s" % (type(e), e), file=sys.stderr)
