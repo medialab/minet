31bdf9d91e930101621d5f7c42ce958aae190f45
Yomguithereal <guillaumeplique@gmail.com>
2020-12-16 21:56:18 +0100

    Adding rough install script

---
+++ scripts/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Functions
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" |
+    grep '"tag_name":' |
+    sed -E 's/.*"([^"]+)".*/\1/'
+}
+
+cleanup() {
+  rm -rf /tmp/minet
+  rm -f /tmp/minet.zip
+  rm -rf /usr/local/bin/minet-dist
+  rm /usr/local/bin/minet
+}
+
+# Variables
+name=$(uname -s)
+latest=$(get_latest_release medialab/minet)
+
+# Generic install script
+if [[ $name == "Darwin" ]]; then
+  cleanup
+  echo "Downloading binaries..."
+  mkdir /tmp/minet
+  curl -L "https://github.com/medialab/minet/releases/download/$latest/macos.zip" > /tmp/minet.zip
+  echo "Installing..."
+  unzip -qq /tmp/minet.zip -d /tmp/minet/
+  rm /tmp/minet.zip
+  mv /tmp/minet /usr/local/bin/minet-dist
+  printf "#!/bin/bash\\n/usr/local/bin/minet-dist/minet" > /usr/local/bin/minet
+  chmod +x /usr/local/bin/minet
+  chmod +x /usr/local/bin/minet-dist/minet
+  echo "minet is now correctly installed!"
+else
+  echo "Installation not supported on $name yet."
+fi
---
+++ scripts/uninstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -rf /usr/local/bin/minet-dist
+rm /usr/local/bin/minet
