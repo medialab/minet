89bf725caf2e8a4ed4be4c1d64bf052e3b476661
Yomguithereal <guillaumeplique@gmail.com>
2021-01-28 16:43:02 +0100

    Working poc

--- minet/twitter/api_scraper.py
+++ minet/twitter/api_scraper.py
@@ -5,16 +5,28 @@
 # Twitter public API "scraper".
 #
 import re
-from urllib.parse import quote
+import time
+import datetime
+from urllib.parse import urlencode, quote
 
-from minet.utils import create_pool, request
+from minet.utils import (
+    create_pool,
+    request,
+    request_json
+)
 from minet.twitter.constants import (
     TWITTER_PUBLIC_API_DEFAULT_TIMEOUT,
-    # TWITTER_PUBLIC_API_AUTH_HEADER
+    TWITTER_PUBLIC_API_AUTH_HEADER
 )
 from minet.twitter.exceptions import (
     TwitterGuestTokenError
 )
+
+# =============================================================================
+# Constants
+# =============================================================================
+TWITTER_PUBLIC_SEARCH_ENDPOINT = 'https://api.twitter.com/2/search/adaptive.json'
+GUEST_TOKEN_COOKIE_PATTERN = re.compile(rb'document\.cookie = decodeURIComponent\("gt=(\d+);')
 
 
 # =============================================================================
@@ -27,9 +39,6 @@ def forge_search_url(query):
     )
 
 
-GUEST_TOKEN_COOKIE_PATTERN = re.compile(rb'document\.cookie = decodeURIComponent\("gt=(\d+);')
-
-
 def extract_guest_token(html):
     match = GUEST_TOKEN_COOKIE_PATTERN.search(html)
 
@@ -39,6 +48,47 @@ def extract_guest_token(html):
     return match.group(1).decode()
 
 
+def ensure_guest_token(method):
+    def wrapped(self, *args, **kwargs):
+
+        # TODO: handle expiry
+        if self.guest_token is None:
+            self.acquire_guest_token()
+
+        return method(self, *args, **kwargs)
+
+    return wrapped
+
+
+def create_cookie_expiration():
+    return datetime.datetime.utcfromtimestamp(time.time() + 10800).strftime('%a, %d %b %Y %H:%M:%S GMT')
+
+
+def forge_search_params(query, count=100, cursor=None):
+    params = {
+        'include_can_media_tag': '1',
+        'include_ext_alt_text': 'true',
+        'include_quote_count': 'true',
+        'include_reply_count': '1',
+        'tweet_mode': 'extended',
+        'include_entities': 'true',
+        'include_user_entities': 'true',
+        'include_ext_media_availability': 'true',
+        'send_error_codes': 'true',
+        'simple_quoted_tweet': 'true',
+        'spelling_corrections': '1',
+        'ext': 'mediaStats,highlightedLabel',
+        'tweet_search_mode': 'live',
+        'count': count,
+        'q': query
+    }
+
+    if cursor is not None:
+        params['cursor'] = cursor
+
+    return urlencode(params, quote_via=quote)
+
+
 # =============================================================================
 # Main class
 # =============================================================================
@@ -46,11 +96,19 @@ class TwitterAPIScraper(object):
     def __init__(self):
         self.http = create_pool(timeout=TWITTER_PUBLIC_API_DEFAULT_TIMEOUT)
         self.guest_token = None
+        self.cookie = None
+
+    # TODO: tenacity
+    def request(self, url):
+        return request(self.http, url, spoof_ua=True)
+
+    def request_json(self, url, headers=None):
+        return request_json(self.http, url, spoof_ua=True, headers=headers)
 
     def acquire_guest_token(self):
         base_url = forge_search_url('test')
 
-        err, response = request(self.http, base_url, spoof_ua=True)
+        err, response = self.request(base_url)
 
         if err or response.status >= 400:
             raise TwitterGuestTokenError('call failed')
@@ -61,3 +119,25 @@ class TwitterAPIScraper(object):
             raise TwitterGuestTokenError('could not parse')
 
         self.guest_token = guest_token
+        self.cookie = (
+            response.headers['Set-Cookie'] +
+            ', gt=%s; Domain=.twitter.com; Path=/; Secure ; Expires=%s' %
+            (guest_token, create_cookie_expiration())
+        )
+
+    @ensure_guest_token
+    def request_search(self, query):
+        params = forge_search_params(query)
+        url = '%s?%s' % (TWITTER_PUBLIC_SEARCH_ENDPOINT, params)
+
+        headers = {
+            'Authorization': TWITTER_PUBLIC_API_AUTH_HEADER,
+            'X-Guest-Token': self.guest_token,
+            'Cookie': self.cookie
+        }
+
+        err, response, data = self.request_json(url, headers=headers)
+
+        print(err)
+        print(response.status)
+        print(data)
