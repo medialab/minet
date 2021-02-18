4a5426e994a93582da8c33a5822fa6841817f8a0
Yomguithereal <guillaumeplique@gmail.com>
2021-02-18 19:31:32 +0100

    Adapting fb url-parse to newer ural Fixes #187

--- minet/cli/facebook/url_parse.py
+++ minet/cli/facebook/url_parse.py
@@ -11,12 +11,21 @@ from ural.facebook import (
     parse_facebook_url,
     FacebookPost,
     FacebookUser,
-    FacebookHandle
+    FacebookGroup,
+    FacebookHandle,
+    FacebookPhoto,
+    FacebookVideo
 )
 
 from minet.cli.utils import open_output_file
 
-REPORT_HEADERS = ['facebook_type', 'facebook_id', 'facebook_handle', 'facebook_normalized_url']
+REPORT_HEADERS = [
+    'facebook_type',
+    'facebook_id',
+    'facebook_full_id',
+    'facebook_handle',
+    'facebook_normalized_url'
+]
 
 
 def facebook_url_parse_action(namespace):
@@ -46,20 +55,41 @@ def facebook_url_parse_action(namespace):
         if parsed is None:
             enricher.writerow(row)
 
-        if isinstance(parsed, FacebookPost):
+        elif isinstance(parsed, FacebookPost):
             enricher.writerow(
                 row,
-                ['post', parsed.id, '', parsed.url]
+                ['post', parsed.id, parsed.full_id or '', '', parsed.url]
             )
 
         elif isinstance(parsed, FacebookHandle):
             enricher.writerow(
                 row,
-                ['handle', '', parsed.handle, parsed.url]
+                ['handle', '', '', parsed.handle, parsed.url]
             )
 
         elif isinstance(parsed, FacebookUser):
             enricher.writerow(
                 row,
-                ['user', parsed.id or '', parsed.handle or '', parsed.url]
+                ['user', parsed.id or '', '', parsed.handle or '', parsed.url]
             )
+
+        elif isinstance(parsed, FacebookGroup):
+            enricher.writerow(
+                row,
+                ['group', parsed.id or '', '', parsed.handle or '', parsed.url]
+            )
+
+        elif isinstance(parsed, FacebookPhoto):
+            enricher.writerow(
+                row,
+                ['photo', parsed.id, '', '', parsed.url]
+            )
+
+        elif isinstance(parsed, FacebookVideo):
+            enricher.writerow(
+                row,
+                ['video', parsed.id, '', '', parsed.url]
+            )
+
+        else:
+            raise TypeError('unknown facebook parse result type!')
