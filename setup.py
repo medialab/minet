04fe9b750bb2f730d63550d8ae4012a9a33e47e0
Yomguithereal <guillaumeplique@gmail.com>
2021-03-04 12:49:03 +0100

    Upgrading minet

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(name='minet',
         'tqdm>=4.31.1',
         'trafilatura>=0.7.0,<0.8',
         'twitwi>=0.4.0',
-        'ural>=0.26.1',
+        'ural>=0.27.0',
         'urllib3[secure]>=1.26.3'
       ],
       entry_points={
