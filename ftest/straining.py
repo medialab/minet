f3ce3469e6532cd307255113f124b788cb28559a
Yomguithereal <guillaumeplique@gmail.com>
2021-03-23 09:48:39 +0100

    ftest.straining

---
+++ ftest/straining.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup, SoupStrainer
+
+html = '<p id="ok">test</p><span>whatever</span>'
+
+def strain(tag, attrs):
+    print(tag, attrs)
+
+strainer = SoupStrainer(strain)
+
+soup = BeautifulSoup(html, 'lxml', parse_only=strainer)
