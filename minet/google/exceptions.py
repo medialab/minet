bfabbfa99e3848f9c9174f83ceb8a80e99f99998
Yomguithereal <guillaumeplique@gmail.com>
2021-03-23 15:22:28 +0100

    Scaffolding minet.google

---
+++ minet/google/exceptions.py
@@ -0,0 +1,21 @@
+# =============================================================================
+# Minet Google Exceptions
+# =============================================================================
+#
+from minet.exceptions import MinetError
+
+
+class GoogleError(MinetError):
+    pass
+
+
+class GoogleSheetsError(GoogleError):
+    pass
+
+
+class GoogleSheetsInvalidTargetError(GoogleSheetsError):
+    pass
+
+
+class GoogleSheetsInvalidContentTypeError(GoogleSheetsError):
+    pass
