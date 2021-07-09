caac26a420f31739b7a2ee00f72724bcc3b3a8dd
Yomguithereal <guillaumeplique@gmail.com>
2021-07-09 17:44:49 +0200

    Fixing tweet scraper retryer logger Related to #309

--- minet/cli/twitter/scrape.py
+++ minet/cli/twitter/scrape.py
@@ -8,13 +8,10 @@ import casanova
 from twitwi.constants import TWEET_FIELDS
 from twitwi import format_tweet_as_csv_row
 
-from minet.utils import prettyprint_seconds, PseudoFStringFormatter
+from minet.utils import PseudoFStringFormatter
 from minet.cli.utils import LoadingBar
 from minet.twitter import TwitterAPIScraper
-from minet.twitter.exceptions import (
-    TwitterPublicAPIRateLimitError,
-    TwitterPublicAPIOverCapacityError
-)
+from minet.twitter.exceptions import TwitterPublicAPIOverCapacityError
 from minet.twitter.constants import ADDITIONAL_TWEET_FIELDS
 
 CUSTOM_FORMATTER = PseudoFStringFormatter()
@@ -49,18 +46,6 @@ def twitter_scrape_action(cli_args):
         keep=cli_args.select
     )
 
-    def before_sleep(retry_state):
-        exc = retry_state.outcome.exception()
-
-        if isinstance(exc, TwitterPublicAPIRateLimitError):
-            loading_bar.inc('tokens')
-
-        else:
-            loading_bar.inc('failures')
-            loading_bar.print(
-                'Failed to call Twitter search. Will retry in %s' % prettyprint_seconds(retry_state.idle_for)
-            )
-
     for row, query in enricher.cells(cli_args.query, with_rows=True):
 
         # Templating?
@@ -76,7 +61,6 @@ def twitter_scrape_action(cli_args):
         iterator = scraper.search(
             query,
             limit=cli_args.limit,
-            before_sleep=before_sleep,
             include_referenced_tweets=cli_args.include_refs,
             with_meta=True
         )
