66fe9ecb4801986e0478f71bcc313039005f3eac
elanhermi <hlne.herman@gmail.com>
2022-02-23 11:04:54 +0100

    Solving git quiproquo

--- minet/cli/twitter/scrape.py
+++ minet/cli/twitter/scrape.py
@@ -1,75 +1,193 @@
 # =============================================================================
-# Minet Twitter Scrape CLI Action
+# Minet Scrape CLI Action
 # =============================================================================
 #
-# Logic of the `tw scrape` action.
+# Logic of the scrape action.
 #
+import csv
+import sys
+import ndjson
 import casanova
-from twitwi.constants import TWEET_FIELDS
-from twitwi import format_tweet_as_csv_row
+from casanova import DictLikeRow
+from termcolor import colored
+from collections import namedtuple
+from os.path import basename, isdir
 
-from minet.utils import PseudoFStringFormatter
-from minet.cli.utils import LoadingBar
-from minet.twitter import TwitterAPIScraper
-from minet.twitter.exceptions import TwitterPublicAPIOverCapacityError
-from minet.twitter.constants import ADDITIONAL_TWEET_FIELDS
+from minet import Scraper
+from minet.multiprocessing import LazyPool
+from minet.exceptions import (
+    DefinitionInvalidFormatError,
+)
+from minet.scrape.exceptions import (
+    InvalidScraperError,
+    CSSSelectorTooComplex,
+    ScraperEvalError,
+    ScraperEvalTypeError,
+    ScraperEvalNoneError
+)
+from minet.cli.reporters import (
+    report_scraper_validation_errors,
+    report_scraper_evaluation_error
+)
+from minet.fs import read_potentially_gzipped_path
+from minet.cli.utils import (
+    die,
+    dummy_csv_file_from_glob,
+    create_report_iterator,
+    LoadingBar
+)
+from minet.cli.constants import DEFAULT_CONTENT_FOLDER
 
-CUSTOM_FORMATTER = PseudoFStringFormatter()
+ScrapeWorkerResult = namedtuple(
+    'ScrapeWorkerResult',
+    ['error', 'items']
+)
+
+PROCESS_SCRAPER = None
 
 
-def format_meta_row(meta):
-    if meta is None:
-        return [''] * len(ADDITIONAL_TWEET_FIELDS)
+def init_process(definition, strain):
+    global PROCESS_SCRAPER
 
-    return [
-        meta.get('intervention_type'),
-        meta.get('intervention_text'),
-        meta.get('intervention_url')
-    ]
+    PROCESS_SCRAPER = Scraper(definition, strain=strain)
 
 
-def twitter_scrape_action(cli_args):
-    scraper = TwitterAPIScraper()
+def worker(payload):
+    row, headers, path, encoding, content, args = payload
+    output_format, plural_separator = args
 
-    # Stats
-    loading_bar = LoadingBar(
-        'Collecting tweets',
-        total=cli_args.limit,
-        unit='tweet',
-        stats={'tokens': 1, 'queries': 0}
-    )
-
-    enricher = casanova.enricher(
-        cli_args.file,
-        cli_args.output,
-        add=TWEET_FIELDS + ADDITIONAL_TWEET_FIELDS,
-        keep=cli_args.select
-    )
-
-    for row, query in enricher.cells(cli_args.query, with_rows=True):
-
-        # Templating?
-        if cli_args.query_template is not None:
-            query = CUSTOM_FORMATTER.format(
-                cli_args.query_template,
-                value=query
-            )
-
-        loading_bar.print('Searching for "%s"' % query)
-        loading_bar.inc('queries')
-
-        iterator = scraper.search_tweets(
-            query,
-            limit=cli_args.limit,
-            include_referenced_tweets=cli_args.include_refs,
-            with_meta=True
-        )
-
+    # Reading from file
+    if content is None:
         try:
-            for tweet, meta in iterator:
-                loading_bar.update()
+            content = read_potentially_gzipped_path(path, encoding=encoding)
+        except (FileNotFoundError, UnicodeDecodeError) as e:
+            return ScrapeWorkerResult(e, None)
 
-                tweet_row = format_tweet_as_csv_row(tweet)
-                enricher.writerow(row, tweet_row + format_meta_row(meta))
-        except TwitterPublicAPIOverCapacityError:
-            loading_bar.die('Got an "Over Capacity" error. Shutting down...')
+    # Building context
+    context = {}
+
+    if row:
+        context['line'] = DictLikeRow(headers, row)
+
+    if path:
+        context['path'] = path
+        context['basename'] = basename(path)
+
+    # Attempting to scrape
+    if output_format == 'csv':
+        items = PROCESS_SCRAPER.as_csv_dict_rows(content, context=context, plural_separator=plural_separator)
+    else:
+        items = PROCESS_SCRAPER.as_records(content, context=context)
+
+    # NOTE: errors will only be raised when we consume the generators created above
+    try:
+        items = list(items)
+    except (ScraperEvalError, ScraperEvalTypeError, ScraperEvalNoneError) as error:
+        return ScrapeWorkerResult(error, None)
+
+    return ScrapeWorkerResult(None, items)
+
+
+def scrape_action(cli_args):
+    if cli_args.glob is None and cli_args.input_dir is None:
+        cli_args.input_dir = DEFAULT_CONTENT_FOLDER
+
+    # Parsing scraper definition
+    try:
+        scraper = Scraper(cli_args.scraper, strain=cli_args.strain)
+    except DefinitionInvalidFormatError:
+        die([
+            'Unknown scraper format!',
+            'It should be a JSON or YAML file.'
+        ])
+    except FileNotFoundError:
+        die('Could not find scraper file!')
+    except InvalidScraperError as error:
+        print('Your scraper is invalid! You need to fix the following errors:', file=sys.stderr)
+        print(file=sys.stderr)
+        sys.stderr.write(report_scraper_validation_errors(error.validation_errors))
+        die()
+    except CSSSelectorTooComplex:
+        die([
+            'Your strainer\'s CSS selector %s is too complex.' % colored(cli_args.strain, 'blue'),
+            'You cannot use relations to create a strainer.',
+            'Try to simplify the selector you passed to --strain.'
+        ])
+
+    if cli_args.validate:
+        print('Your scraper is valid.', file=sys.stderr)
+        sys.exit(0)
+
+    if scraper.headers is None and cli_args.format == 'csv':
+        die([
+            'Your scraper does not yield tabular data.',
+            'Try changing it or setting --format to "jsonl".'
+        ])
+
+    loading_bar = LoadingBar(
+        desc='Scraping pages',
+        total=cli_args.total,
+        unit='page'
+    )
+
+    worker_args = (
+        cli_args.format,
+        cli_args.separator
+    )
+
+    def on_irrelevant_row(reason, row, i):
+        loading_bar.print('Row n°{n} could not be processed: {reason}'.format(n=i + 1, reason=reason))
+        loading_bar.update()
+
+    input_data = cli_args.report
+
+    if cli_args.glob is not None:
+        input_data = dummy_csv_file_from_glob(cli_args.glob, cli_args.input_dir)
+
+    reader = casanova.reader(input_data)
+
+    if (
+        cli_args.glob is None and
+        'raw_contents' not in reader.headers and
+        not isdir(cli_args.input_dir)
+    ):
+        loading_bar.die([
+            'Could not find the "%s" directory!' % cli_args.input_dir,
+            'Did you forget to specify it with -i/--input-dir?'
+        ])
+
+    files = create_report_iterator(
+        cli_args,
+        reader,
+        worker_args=worker_args,
+        on_irrelevant_row=on_irrelevant_row
+    )
+
+    if cli_args.format == 'csv':
+        output_writer = csv.DictWriter(cli_args.output, fieldnames=scraper.headers)
+        output_writer.writeheader()
+    else:
+        output_writer = ndjson.writer(cli_args.output)
+
+    pool = LazyPool(
+        cli_args.processes,
+        initializer=init_process,
+        initargs=(scraper.definition, cli_args.strain)
+    )
+
+    loading_bar.update_stats(p=pool.processes)
+
+    with pool:
+        for error, items in pool.imap_unordered(worker, files):
+            loading_bar.update()
+
+            if error is not None:
+                if isinstance(error, (ScraperEvalError, ScraperEvalTypeError, ScraperEvalNoneError)):
+                    loading_bar.print(report_scraper_evaluation_error(error), end='')
+                else:
+                    loading_bar.print(error, repr(error))
+                loading_bar.inc('errors')
+                continue
+
+            for item in items:
+                output_writer.writerow(item)
