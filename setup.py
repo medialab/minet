fb64638c7d05952e423959e4f86ade9cb2509082
Yomguithereal <guillaumeplique@gmail.com>
2022-10-14 13:09:55 +0200

    Upgrading casanova to have a way to ignore null bytes by default

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         "beautifulsoup4>=4.7.1",
         "browser-cookie3==0.16.1",
-        "casanova>=0.18.0,<0.19",
+        "casanova>=0.19.0,<0.20",
         "cchardet>=2.1.7",
         "colorama>=0.4.0",
         "dateparser>=1.1.1",
