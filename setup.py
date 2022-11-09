ec62612ed8fe62cc2e6b037745d0220e3081d5e8
Yomguithereal <guillaumeplique@gmail.com>
2022-11-09 10:46:08 +0100

    Bumping browser_cookie3

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     packages=find_packages(exclude=["ftest", "scripts", "test"]),
     install_requires=[
         "beautifulsoup4>=4.7.1",
-        "browser-cookie3==0.16.1",
+        "browser-cookie3==0.16.2",
         "casanova>=0.19.1,<0.20",
         "cchardet>=2.1.7",
         "colorama>=0.4.0",
