5a04ad2e8857ad5a61da03bec4664dd32b876366
Yomguithereal <guillaumeplique@gmail.com>
2023-02-19 21:14:07 +0100

    Starting to work on features

--- ftest/loading_bar.py
+++ ftest/loading_bar.py
@@ -6,9 +6,17 @@ from minet.cli.loading_bar import LoadingBar
 
 N = 1_000
 try:
-    with LoadingBar(title="Processing", total=N, unit="apples") as loading_bar:
+    with LoadingBar(
+        title="Processing", total=N, unit="apples", features=[]
+    ) as loading_bar:
         for i in range(N):
             time.sleep(0.005)
+
+            # loading_bar.update(label="Currently processing %i" % i)
+
+            if i % 100 == 0:
+                print("Reached", i)
+
             loading_bar.advance()
 except KeyboardInterrupt:
     sys.exit(1)
--- minet/cli/loading_bar.py
+++ minet/cli/loading_bar.py
@@ -7,6 +7,7 @@
 from typing import Optional
 from rich.live import Live
 from rich.text import Text
+from rich.table import Table
 from rich.progress import (
     Progress,
     ProgressColumn,
@@ -16,6 +17,7 @@ from rich.progress import (
     TaskProgressColumn,
     Task,
 )
+from rich.console import Group
 from rich._spinners import SPINNERS
 from about_time import HumanDuration, HumanThroughput
 from ebbe import format_int
@@ -91,7 +93,7 @@ class ThroughputColumn(ProgressColumn):
 
         eta = ""
 
-        if task.total is not None:
+        if task.total is not None and task.time_remaining:
             remaining = HumanDuration(task.time_remaining)
             eta = f" eta: {remaining}"
 
@@ -106,13 +108,30 @@ class LoadingBar(object):
         title: Optional[str] = None,
         unit: Optional[str] = None,
         total: Optional[int] = None,
+        features=[],
     ):
         self.title = title
         self.unit = unit
         self.total = total
+        self.features = set(features)
 
         self.bar_column = None
         self.spinner_column = None
+        self.upper_line = None
+        self.upper_line_task = None
+        self.sub_progress = None
+        self.sub_task = None
+
+        self.table = Table.grid(expand=True)
+
+        if "label" in self.features or "stats" in self.features:
+            self.upper_line = Progress(
+                TextColumn("[progress.description]{task.description}")
+            )
+            self.upper_line_task = self.upper_line.add_task(
+                description="Coucou", total=None
+            )
+            self.table.add_row(self.upper_line)
 
         if total is not None:
             self.bar_column = BarColumn()
@@ -121,7 +140,9 @@ class LoadingBar(object):
                 TextColumn("[progress.description]{task.description}"),
                 self.bar_column,
                 CompletionColumn(unit=self.unit),
-                SpinnerColumn("dots", style=None, finished_text="▰"),
+                SpinnerColumn("dots", style=None, finished_text="·")
+                if "secondary" not in self.features
+                else None,
                 TaskProgressColumn("[progress.percentage][{task.percentage:>3.0f}%]"),
                 TimeElapsedColumn(),
                 ThroughputColumn(),
@@ -138,11 +159,29 @@ class LoadingBar(object):
                 ThroughputColumn(),
             ]
 
+        columns = [c for c in columns if c]
+
         self.progress = Progress(*columns, console=console)
-        self.live = Live(self.progress, refresh_per_second=10, console=console)
+        self.table.add_row(self.progress)
 
         self.task = self.progress.add_task(
             description=self.title, total=self.total, fields={"unit": self.unit}
+        )
+
+        if "secondary" in self.features:
+            self.sub_progress = Progress(
+                SpinnerColumn("dots", style="", finished_text="·"),
+                TextColumn("{task.description}"),
+            )
+            self.sub_task = self.sub_progress.add_task(
+                description="Sub task", total=None
+            )
+            self.table.add_row(self.sub_progress)
+
+        self.live = Live(
+            self.table,
+            refresh_per_second=10,
+            console=console,
         )
 
     def __enter__(self):
@@ -152,6 +191,9 @@ class LoadingBar(object):
     def __exit__(self, exc_type, exc_value, exc_traceback):
 
         if exc_type is not None:
+            # NOTE: cursor 1up
+            console.file.write("\x1b[1A")
+
             if self.bar_column is not None:
                 self.bar_column.complete_style = "warning"
             if self.spinner_column is not None:
@@ -165,3 +207,11 @@ class LoadingBar(object):
 
     def advance(self, count=1):
         self.progress.update(self.task, advance=count)
+
+    def update(self, count=None, label=None):
+        if count is not None:
+            self.advance(count)
+
+        if label is not None:
+            assert self.upper_line is not None
+            self.upper_line.update(self.upper_line_task, description=label)
