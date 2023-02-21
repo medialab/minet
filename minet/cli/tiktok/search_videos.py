730f0698254241de65ad8d6279a03f9acf998615
Yomguithereal <guillaumeplique@gmail.com>
2023-02-21 08:29:20 +0100

    Porting tk cmds

--- minet/cli/tiktok/search_videos.py
+++ minet/cli/tiktok/search_videos.py
@@ -4,34 +4,30 @@
 #
 # Logic of the `tiktok search-videos` action.
 #
-import casanova
 from itertools import islice
 
-from minet.cli.utils import LoadingBar
+from minet.cli.utils import with_enricher_and_loading_bar
 from minet.tiktok import TiktokAPIScraper
 from minet.tiktok.constants import TIKTOK_VIDEO_CSV_HEADERS
 
 
-def action(cli_args):
-    enricher = casanova.enricher(
-        cli_args.input,
-        cli_args.output,
-        add=TIKTOK_VIDEO_CSV_HEADERS,
-    )
-
-    loading_bar = LoadingBar("Retrieving videos", unit="querie", stats={"videos": 0})
-
+@with_enricher_and_loading_bar(
+    headers=TIKTOK_VIDEO_CSV_HEADERS,
+    title="Searching videos",
+    unit="queries",
+    nested=True,
+    sub_unit="videos",
+)
+def action(cli_args, enricher, loading_bar):
     client = TiktokAPIScraper(cookie=cli_args.cookie)
 
     for row, query in enricher.cells(cli_args.column, with_rows=True):
-        loading_bar.update()
+        with loading_bar.nested_task(query):
+            generator = client.search_videos(query)
 
-        generator = client.search_videos(query)
+            if cli_args.limit:
+                generator = islice(generator, cli_args.limit)
 
-        if cli_args.limit:
-            generator = islice(generator, cli_args.limit)
-
-        for video in generator:
-            enricher.writerow(row, video.as_csv_row())
-
-            loading_bar.inc("videos")
+            for video in generator:
+                enricher.writerow(row, video.as_csv_row())
+                loading_bar.nested_advance()
