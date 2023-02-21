f7e5974551db56f5d68decc1af98f550d20b980a
Yomguithereal <guillaumeplique@gmail.com>
2023-02-21 08:16:51 +0100

    Porting insta cmds

--- minet/cli/instagram/user_posts.py
+++ minet/cli/instagram/user_posts.py
@@ -4,10 +4,9 @@
 #
 # Logic of the `instagram user-posts` action.
 #
-import casanova
 from itertools import islice
 
-from minet.cli.utils import LoadingBar
+from minet.cli.utils import with_enricher_and_loading_bar
 from minet.cli.instagram.utils import with_instagram_fatal_errors
 from minet.instagram import InstagramAPIScraper
 from minet.instagram.constants import INSTAGRAM_USER_POST_CSV_HEADERS
@@ -19,43 +18,41 @@ from minet.instagram.exceptions import (
 
 
 @with_instagram_fatal_errors
-def action(cli_args):
+@with_enricher_and_loading_bar(
+    headers=INSTAGRAM_USER_POST_CSV_HEADERS,
+    title="Scraping posts",
+    unit="users",
+    nested=True,
+    sub_unit="posts",
+)
+def action(cli_args, enricher, loading_bar):
     client = InstagramAPIScraper(cookie=cli_args.cookie)
 
-    enricher = casanova.enricher(
-        cli_args.input,
-        cli_args.output,
-        add=INSTAGRAM_USER_POST_CSV_HEADERS,
-    )
-
-    loading_bar = LoadingBar("Retrieving posts", unit="user", stats={"posts": 0})
-
     for i, (row, user) in enumerate(enricher.cells(cli_args.column, with_rows=True)):
-        loading_bar.update()
+        with loading_bar.nested_task(user):
+            try:
+                generator = client.user_posts(user)
 
-        try:
-            generator = client.user_posts(user)
+                if cli_args.limit:
+                    generator = islice(generator, cli_args.limit)
 
-            if cli_args.limit:
-                generator = islice(generator, cli_args.limit)
+                for post in generator:
+                    enricher.writerow(row, post.as_csv_row())
+                    loading_bar.nested_advance()
 
-            for post in generator:
-                enricher.writerow(row, post.as_csv_row())
+            except InstagramInvalidTargetError:
+                loading_bar.print(
+                    "Given user (line %i) is probably not an Instagram user: %s"
+                    % (i, user)
+                )
 
-                loading_bar.inc("posts")
+            except InstagramPrivateOrNonExistentAccountError:
+                loading_bar.print(
+                    "Given user (line %i) is probably a private Instagram account or is not an Instagram user: %s"
+                    % (i, user)
+                )
 
-        except InstagramInvalidTargetError:
-            loading_bar.print(
-                "Given user (line %i) is probably not an Instagram user: %s" % (i, user)
-            )
-
-        except InstagramPrivateOrNonExistentAccountError:
-            loading_bar.print(
-                "Given user (line %i) is probably a private Instagram account or is not an Instagram user: %s"
-                % (i, user)
-            )
-
-        except InstagramNoPublicationError:
-            loading_bar.print(
-                "Given user (line %i) has probably no publication: %s" % (i, user)
-            )
+            except InstagramNoPublicationError:
+                loading_bar.print(
+                    "Given user (line %i) has probably no publication: %s" % (i, user)
+                )
