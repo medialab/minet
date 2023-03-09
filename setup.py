fb19080284879b94ae0b5898479b0cf70f962429
Yomguithereal <guillaumeplique@gmail.com>
2023-03-09 13:44:04 +0100

    Upgrading casanova

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         "about-time>=4,<5",
         "beautifulsoup4>=4.7.1,<5",
         "browser-cookie3==0.17.0",
-        "casanova>=1.6.3,<2",
+        "casanova>=1.7.2,<1.8",
         "charset-normalizer>=3,<4",
         "dateparser>=1.1.1",
         "ebbe>=1.11.1,<2",
