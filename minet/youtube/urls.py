ee5e7f492c7c3581192d8b9b18039e005b170419
Yomguithereal <guillaumeplique@gmail.com>
2023-05-17 17:47:23 +0200

    Finishing YouTubeAPIURLFormatter

--- minet/youtube/urls.py
+++ minet/youtube/urls.py
@@ -5,20 +5,19 @@ from ural import URLFormatter
 from minet.youtube.constants import (
     YOUTUBE_API_BASE_URL,
     YOUTUBE_API_MAX_VIDEOS_PER_CALL,
+    YOUTUBE_API_MAX_COMMENTS_PER_CALL,
+    YOUTUBE_API_DEFAULT_SEARCH_ORDER,
 )
 
 
 class YouTubeAPIURLFormatter(URLFormatter):
     BASE_URL = YOUTUBE_API_BASE_URL
 
-    def __init__(self):
-        def format_arg_value(k, v):
-            if isinstance(v, Iterable):
-                return ",".join(v)
+    def format_arg_value(self, _, v):
+        if isinstance(v, list):
+            return ",".join(v)
 
-            return v
-
-        super().__init__(format_arg_value=format_arg_value)
+        return v
 
     def playlist_videos(self, playlist_id: str, token: Optional[str]) -> str:
         return self.format(
@@ -35,4 +34,60 @@ class YouTubeAPIURLFormatter(URLFormatter):
         return self.format(
             path="videos",
             args={"id": ids, "part": ["snippet", "statistics", "contentDetails"]},
+        )
+
+    def channels(self, ids: Iterable[str]) -> str:
+        return self.format(
+            path="channels",
+            args={
+                "id": ids,
+                "part": [
+                    "snippet",
+                    "statistics",
+                    "contentDetails",
+                    "topicDetails",
+                    "brandingSettings",
+                    "status",
+                ],
+            },
+        )
+
+    def search(
+        self,
+        query: str,
+        order: str = YOUTUBE_API_DEFAULT_SEARCH_ORDER,
+        token: Optional[str] = None,
+    ) -> str:
+        return self.format(
+            path="search",
+            args={
+                "part": "snippet",
+                "maxResults": YOUTUBE_API_MAX_VIDEOS_PER_CALL,
+                "q": query,
+                "type": "video",
+                "order": order,
+                "pageToken": token,
+            },
+        )
+
+    def comments(self, video_id: str, token: Optional[str] = None) -> str:
+        return self.format(
+            path="commentThreads",
+            args={
+                "videoId": video_id,
+                "part": ["snippet", "replies"],
+                "maxResults": YOUTUBE_API_MAX_COMMENTS_PER_CALL,
+                "pageToken": token,
+            },
+        )
+
+    def replies(self, comment_id: str, token: Optional[str] = None) -> str:
+        return self.format(
+            path="comments",
+            args={
+                "parentId": comment_id,
+                "part": "snippet",
+                "maxResults": YOUTUBE_API_MAX_COMMENTS_PER_CALL,
+                "pageToken": token,
+            },
         )
