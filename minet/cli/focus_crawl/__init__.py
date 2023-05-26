388567fadce81f5bfba9325119bd782da955454b
César <cesarpichon@gmail.com>
2023-05-26 16:07:27 +0200

    adding cli focus crawl

---
+++ minet/cli/focus_crawl/__init__.py
@@ -0,0 +1,80 @@
+from minet.cli.argparse import command
+
+from minet.constants import DEFAULT_THROTTLE
+
+FOCUS_CRAWL_COMMAND = command(
+    "focus",
+    "minet.cli.focus_crawl.focus_crawl",
+    title="Minet Focus Crawl Command",
+    description="""
+        Minet crawl feature with the possibility
+        to use regular expressions or keywords to
+        filter content.
+    """,
+    epilog="""
+        Examples:
+
+          Running a simple crawler:
+            $ minet focus-crawl ......
+    """,
+    no_output=True,
+    variadic_input={"dummy_column": "url"},
+    arguments=[
+        {
+            "flags": ["-r", "--regex-content"],
+            "help": "Regex used to filter fetched content.",
+            "default": None
+        },
+        {
+            "flags": ["-u", "--regex-url"],
+            "help": "Regex used to filter URLs added to crawler's queue.",
+            "default": None
+        },
+        {
+            "flag": "--on-text",
+            "help": "Perform regex match on extracted text content instead of html content.",
+            "action": "store_true",
+        },
+        {
+            "flags": ["-m", "--max-depth"],
+            "help": "Max depth of the crawling exploration.",
+            "default": 3
+        },
+        {
+            "flag": "--uninteresting-continue",
+            "help": "Explore whether content is interesting or not.",
+            "action": "store_true"
+        },
+        {
+            "flag": "--target-html",
+            "help": "Add URLs to the crawler queue only if they seem to lead to a HTML content",
+            "action": "store_true",
+        },
+        {
+            "flag": "--keep-uninteresting",
+            "help": "Add to exported data the results judged uninteresting by the algorithm",
+            "action": "store_true",
+        },
+        {
+            "flags": ["-O", "--output-dir"],
+            "help": "Output directory.",
+            "default": "focus_crawl"
+        },
+        {
+            "flag": "--resume",
+            "help": "Whether to resume an interrupted crawl.",
+            "action": "store_true",
+        },
+        {
+            "flag": "--dump-queue",
+            "help": "Print the contents of the persistent queue. (This is for debug only, don't use this flag unless you know what you are doing).",
+            "action": "store_true",
+        },
+        {
+            "flag": "--throttle",
+            "help": "Time to wait - in seconds - between 2 calls to the same domain.",
+            "type": float,
+            "default": DEFAULT_THROTTLE,
+        },
+    ]
+)
