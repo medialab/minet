09df2b4415247b14340631721e4554adff70385f
César <cesarpichon@gmail.com>
2023-06-15 16:44:58 +0200

    adding dump graph commands

--- minet/cli/commands.py
+++ minet/cli/commands.py
@@ -1,7 +1,7 @@
 from minet.cli.cookies import COOKIES_COMMAND
 from minet.cli.crawl import CRAWL_COMMAND
 from minet.cli.focus_crawl import FOCUS_CRAWL_COMMAND
-from minet.cli.dump import DUMP_QUEUE_COMMAND, DUMP_URLS_COMMAND
+from minet.cli.dump import DUMP_QUEUE_COMMAND, DUMP_URLS_COMMAND, DUMP_GRAPH_COMMAND
 from minet.cli.extract import EXTRACT_COMMAND
 from minet.cli.fetch import FETCH_COMMAND, RESOLVE_COMMAND
 from minet.cli.scrape import SCRAPE_COMMAND
@@ -28,6 +28,7 @@ MINET_COMMANDS = [
     FOCUS_CRAWL_COMMAND,
     DUMP_QUEUE_COMMAND,
     DUMP_URLS_COMMAND,
+    DUMP_GRAPH_COMMAND,
     EXTRACT_COMMAND,
     FETCH_COMMAND,
     RESOLVE_COMMAND,
--- minet/cli/dump/__init__.py
+++ minet/cli/dump/__init__.py
@@ -1,4 +1,5 @@
-from minet.cli.argparse import command
+from minet.cli.argparse import command, InputAction
+from sys import stdin
 
 DUMP_QUEUE_COMMAND = command(
     "dump-queue",
@@ -35,5 +36,30 @@ DUMP_URLS_COMMAND = command(
     """,
     arguments=[
         {"name": "urls_dir", "help": "Path to the url store's directory."},
+    ],
+)
+
+DUMP_GRAPH_COMMAND = command(
+    "dump-graph",
+    "minet.cli.dump.dump_graph",
+    title="Minet Dump Graph Command",
+    description="""
+        Debug command that can be used to dump a crawler's graph from
+        a jobs csv and visual it as a network.
+
+        The export file is in json with a Graphology format.
+    """,
+    epilog="""
+        Examples:
+
+        . Dumping a graph file from crawler jobs:
+            $ minet dump-graph ./jobs.csv > graph.json
+    """,
+    arguments=[
+        {
+            "name": "file",
+            "help": "Path to the url store's directory.",
+            "action": InputAction,
+        },
     ],
 )
