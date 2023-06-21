78e0f2b5dccdafc87a6e83d7ec04d80cb30ad0a6
Yomguithereal <guillaumeplique@gmail.com>
2023-06-21 18:08:35 +0200

    More tests

--- ftest/playwright.py
+++ ftest/playwright.py
@@ -1,23 +1,55 @@
 import asyncio
-from webdriver_manager.firefox import GeckoDriverManager
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
 from playwright.async_api import async_playwright
+from quenouille import imap_unordered
 
 # ref: https://github.com/microsoft/playwright-python/issues/342
 # ref: https://github.com/microsoft/playwright/blob/main/packages/playwright-core/src/server/registry/index.ts
 
+URLS = [
+    "https://www.lemonde.fr/",
+    "https://lefigaro.fr",
+    "https://liberation.fr",
+    "https://mediapart.fr",
+]
+
 
 async def main():
-    driver = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+    loop = asyncio.get_event_loop()
 
     async with async_playwright() as playwright:
-        print("before launch", playwright.chromium.executable_path)
-        browser = await playwright.chromium.launch(headless=True, timeout=5000)
-        print("after launch")
-        page = await browser.new_page()
-        await page.goto("https://echojs.com")
+        browser = await playwright.chromium.launch(
+            headless=True, timeout=5000, args=["--remote-debugging-port=9222"]
+        )
+
+        connection = await playwright.chromium.connect_over_cdp("http://localhost:9222")
+        page = await connection.new_page()
+        await page.goto("https://lemonde.fr")
         print(await page.title())
+        await connection.close()
+        print(connection)
+        raise RuntimeError
+
+        async def get_page_title(url: str) -> str:
+            page = await browser.new_page()
+            await page.goto(url)
+
+            title = await page.title()
+
+            await page.close()
+
+            return title
+
+        def worker(url: str) -> str:
+            future = asyncio.run_coroutine_threadsafe(get_page_title(url), loop)
+            return future.result()
+
+        for result in imap_unordered(URLS, worker, 3):
+            print(result)
+
+        # for url in URLS:
+        #     print(url)
+        #     print(await get_page_title(url))
+
         await browser.close()
 
 
