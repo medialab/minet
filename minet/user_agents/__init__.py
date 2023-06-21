190cfa10f218951491ee2ddaec9ad173cb901134
César <cesarpichon@gmail.com>
2023-06-21 16:07:29 +0200

    adding weighted random user agents choice

--- minet/user_agents/__init__.py
+++ minet/user_agents/__init__.py
@@ -1,6 +1,8 @@
 import json
 import urllib3
 import codecs
+import random as rd
+from functools import reduce
 from os.path import dirname, join
 
 import minet.user_agents.data as data
@@ -20,33 +22,49 @@ def useragentsme():
     endpoint = "https://www.useragents.me/api"
     response = download(endpoint)
     results = json.loads(response)
-    for row in results.get("data", []):
-        yield row["ua"]
+    return [
+        (row["pct"], row["ua"])
+        for row in results.get("data", [])
+        if not "Trident" in row["ua"]
+    ]
+
+def calculate_prob(probs):
+    d = 0
+    for p in probs:
+        d = d + p
+        yield d
 
 def update(transient=False):
     try:
         agents = useragentsme()
         output_path = join(dirname(__file__), "data.py")
 
-        data.USER_AGENTS = [a for a in agents if not "Trident" in a]
+        data.USER_AGENTS = [ua for (_, ua) in agents]
+
+        probs = [pct for (pct, _) in agents]
+        data.USER_AGENTS_PROB = [p for p in calculate_prob(probs)]
 
         if transient:
             return
 
         with codecs.open(output_path, "w", encoding="utf-8") as f:
             f.write("# coding: utf-8\n\n")
+
             f.write("USER_AGENTS = [\n")
             for ua in data.USER_AGENTS:
                 eua = ua.replace('"', r'\"')
                 f.write("\t\"%s\",\n" % eua)
             f.write("]\n\n")
-            f.write("LAST_USED_AGENT = 0\n")
+
+            f.write("USER_AGENTS_PROB = [\n")
+            for pct in data.USER_AGENTS_PROB:
+                f.write("\t%f,\n" % pct)
+            f.write("]\n\n")
+
     except:
         raise TypeError("Unable to get new useragents")
 
-def get_useragent(new=False):
-    if new:
-        size = len(data.USER_AGENTS)
-        data.LAST_USED_AGENT = (data.LAST_USED_AGENT + 1 ) % size
-    return data.USER_AGENTS[data.LAST_USED_AGENT]
-
+def get_useragent(random=False):
+    if random:
+        return rd.choices(data.USER_AGENTS, cum_weights=data.USER_AGENTS_PROB, k=1)[0]
+    return data.USER_AGENTS[0]
