311edd5e10d22fb4e39cc1b41ef1b4b750cf39c7
Yomguithereal <guillaumeplique@gmail.com>
2023-06-23 11:37:16 +0200

    Starting to mix asyncio and threading

--- ftest/playwright.py
+++ ftest/playwright.py
@@ -1,6 +1,7 @@
 import asyncio
 from playwright.async_api import async_playwright
 from quenouille import imap_unordered
+from threading import Thread, Event, current_thread
 
 # ref: https://github.com/microsoft/playwright-python/issues/342
 # ref: https://github.com/microsoft/playwright/blob/main/packages/playwright-core/src/server/registry/index.ts
@@ -13,44 +14,109 @@ URLS = [
 ]
 
 
-async def main():
-    loop = asyncio.get_event_loop()
+class ThreadsafePlaywrightBrowser:
+    def __init__(self) -> None:
+        self.loop = asyncio.new_event_loop()
+        self.start_event = Event()
+        self.thread = Thread(target=self.worker)
 
-    async with async_playwright() as playwright:
-        browser = await playwright.chromium.launch(
-            headless=True, timeout=5000, args=["--remote-debugging-port=9222"]
-        )
+        # NOTE: can be wrapped
+        self.thread.start()
+        self.start_event.wait()
 
-        connection = await playwright.chromium.connect_over_cdp("http://localhost:9222")
-        page = await connection.new_page()
-        await page.goto("https://lemonde.fr")
-        print(await page.title())
-        await connection.close()
-        print(connection)
-        raise RuntimeError
+    async def start_playwright(self) -> None:
+        self.playwright = await async_playwright().start()
+        self.browser = await self.playwright.chromium.launch(headless=True)
 
-        async def get_page_title(url: str) -> str:
-            page = await browser.new_page()
-            await page.goto(url)
+    async def stop_playwright(self) -> None:
+        # NOTE: we need to make sure those were actually launched...
+        print(self.browser, self.playwright)
+        await self.browser.close()
+        print("browser closed")
+        await self.playwright.stop()
+        print("playwright closed")
 
-            title = await page.title()
+    def stop(self) -> None:
+        # tasks = asyncio.all_tasks(self.loop)
+        # print(tasks)
+        # NOTE: need to cancel ongoing tasks probably
+        # future = asyncio.run_coroutine_threadsafe(self.stop_playwright(), self.loop)
+        # future.result()
+        self.loop.call_soon_threadsafe(self.loop.stop)
+        print("joining", current_thread())
+        self.thread.join()
 
-            await page.close()
+    def worker(self):
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_until_complete(self.start_playwright())
+        self.start_event.set()
+        try:
+            self.loop.run_forever()
+        finally:
+            self.loop.run_until_complete(self.loop.shutdown_asyncgens())
+        print("finished looping forever", current_thread())
+        self.loop.run_until_complete(self.stop_playwright())
 
-            return title
+    async def get_page_title(self, url: str) -> str:
+        page = await self.browser.new_page()
+        await page.goto(url)
 
-        def worker(url: str) -> str:
-            future = asyncio.run_coroutine_threadsafe(get_page_title(url), loop)
-            return future.result()
+        title = await page.title()
 
-        for result in imap_unordered(URLS, worker, 3):
-            print(result)
+        await page.close()
 
-        # for url in URLS:
-        #     print(url)
-        #     print(await get_page_title(url))
+        return title
 
-        await browser.close()
+    def get_page_title_sync(self, url: str) -> str:
+        return asyncio.run_coroutine_threadsafe(
+            self.get_page_title(url), self.loop
+        ).result()
 
 
-asyncio.run(main())
+browser = ThreadsafePlaywrightBrowser()
+# print(browser.get_page_title_sync(URLS[0]))
+
+browser.stop()
+
+
+# async def main():
+#     loop = asyncio.get_event_loop()
+
+#     async with async_playwright() as playwright:
+#         browser = await playwright.chromium.launch(
+#             headless=True, timeout=5000, args=["--remote-debugging-port=9222"]
+#         )
+
+#         connection = await playwright.chromium.connect_over_cdp("http://localhost:9222")
+#         page = await connection.new_page()
+#         await page.goto("https://lemonde.fr")
+#         print(await page.title())
+#         await connection.close()
+#         print(connection)
+#         raise RuntimeError
+
+#         async def get_page_title(url: str) -> str:
+#             page = await browser.new_page()
+#             await page.goto(url)
+
+#             title = await page.title()
+
+#             await page.close()
+
+#             return title
+
+#         def worker(url: str) -> str:
+#             future = asyncio.run_coroutine_threadsafe(get_page_title(url), loop)
+#             return future.result()
+
+#         for result in imap_unordered(URLS, worker, 3):
+#             print(result)
+
+#         # for url in URLS:
+#         #     print(url)
+#         #     print(await get_page_title(url))
+
+#         await browser.close()
+
+
+# asyncio.run(main())
