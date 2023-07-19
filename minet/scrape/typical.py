ab4f9fc2e0aade32b54dc0e68987d768606500dd
César <cesarpichon@gmail.com>
2023-07-19 11:07:10 +0200

    Adding typical RSS scraper
    Fix #827

--- minet/scrape/typical.py
+++ minet/scrape/typical.py
@@ -4,7 +4,7 @@ from minet.types import AnyScrapableTarget
 from bs4 import SoupStrainer, BeautifulSoup
 from casanova import CSVSerializer
 from urllib.parse import urljoin
-from ural import should_follow_href
+from ural import should_follow_href, could_be_rss
 
 from minet.scrape.analysis import ScraperAnalysisOutputType
 from minet.scrape.utils import ensure_soup
@@ -129,6 +129,35 @@ class MetasScraper(NamedScraper):
         return metas
 
 
+class RssScraper(NamedScraper):
+    name = "rss"
+    fieldnames = ["rss_url"]
+    plural = True
+    output_type = "list"
+    strainer = SoupStrainer(name=["a", "link"])
+
+    def scrape(self, soup: BeautifulSoup, context=None):
+        rss_urls = []
+        base_url = context.get("url") if context is not None else None
+        for link in soup.find_all():
+            if link.name == "link":
+                type_attr = link.attrs.get("type", None)
+                if (
+                    type_attr == "application/rss+xml"
+                    or type_attr == "application/atom+xml"
+                ):
+                    href = link.attrs.get("href", None)
+                    if href:
+                        rss_urls.append(urljoin(base_url, href))
+            else:
+                href = link.attrs.get("href", None)
+                url = urljoin(base_url, href)
+                if could_be_rss(url):
+                    rss_urls.append(url)
+        return rss_urls
+
+
 TYPICAL_SCRAPERS = {
-    s.name: s for s in [TitleScraper, CanonicalScraper, UrlsScraper, MetasScraper]
+    s.name: s
+    for s in [TitleScraper, CanonicalScraper, UrlsScraper, MetasScraper, RssScraper]
 }
