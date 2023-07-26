51666839a1c85f9b4ea7f5a1c804a5fca382081e
Yomguithereal <guillaumeplique@gmail.com>
2023-07-26 15:44:32 +0200

    Drafting setup_curl_handle to make switch to libcurl multi interface easier

--- minet/pycurl.py
+++ minet/pycurl.py
@@ -1,4 +1,4 @@
-from typing import Optional, Dict, List, Tuple, Union
+from typing import Optional, Dict, Union
 from minet.types import AnyTimeout, RedirectionStack, Redirection, HTTPHeaderDict
 
 import pycurl
@@ -8,7 +8,6 @@ from dataclasses import dataclass
 from ebbe import format_repr, format_filesize
 from threading import Event
 from urllib3 import Timeout
-from ebbe import without_last
 from urllib.parse import urljoin
 from urllib3.util.url import parse_url
 
@@ -72,13 +71,12 @@ def coerce_error(
     return PycurlError(error)
 
 
-# TODO: body
-# TODO: decompress?
-# TODO: invalid status error (pycurl has a way I think)?
-# TODO: error serialization, error retrying conversion
-# TODO: pool of curl handles with multi (tricks from https://github.com/tornadoweb/tornado/blob/master/tornado/curl_httpclient.py)
-def request_with_pycurl(
+def setup_curl_handle(
+    curl: pycurl.Curl,
     url: str,
+    response_headers: HTTPHeaderDict,
+    buffer: BytesIO,
+    stack: RedirectionStack,
     method: str = "GET",
     headers: Optional[Dict[str, str]] = None,
     follow_redirects: bool = True,
@@ -86,13 +84,7 @@ def request_with_pycurl(
     timeout: Optional[AnyTimeout] = None,
     cancel_event: Optional[Event] = None,
     verbose: bool = False,
-) -> PycurlResult:
-    # Preemptive cancellation
-    if cancel_event is not None and cancel_event.is_set():
-        raise CancelledRequestError
-
-    curl = pycurl.Curl()
-    buffer = BytesIO()
+) -> None:
 
     # Basics
     curl.setopt(pycurl.URL, sanitize_url(url))
@@ -108,11 +100,21 @@ def request_with_pycurl(
     # Method
     method = method.upper()
 
-    if method == "POST":
+    curl.setopt(pycurl.HTTPGET, False)
+    curl.setopt(pycurl.NOBODY, False)
+    curl.setopt(pycurl.POST, False)
+    curl.setopt(pycurl.PUT, False)
+    curl.unsetopt(pycurl.CUSTOMREQUEST)
+
+    if method == "GET":
+        curl.setopt(pycurl.HTTPGET, True)
+    elif method == "HEAD":
+        curl.setopt(pycurl.NOBODY, True)
+    elif method == "POST":
         curl.setopt(pycurl.POST, True)
     elif method == "PUT":
         curl.setopt(pycurl.PUT, True)
-    elif method != "GET":
+    else:
         curl.setopt(pycurl.CUSTOMREQUEST, method)
 
     # Timeout
@@ -152,9 +154,7 @@ def request_with_pycurl(
 
     # Reading headers
     current_url = url
-    response_headers = HTTPHeaderDict()
     expecting_location_with_status: Optional[int] = None
-    locations: List[Tuple[str, int]] = []
 
     def header_function(header_line):
         nonlocal current_url
@@ -183,7 +183,13 @@ def request_with_pycurl(
         value = value.strip()
 
         if expecting_location_with_status is not None and name.lower() == "location":
-            locations.append((current_url, expecting_location_with_status))
+            stack.append(
+                Redirection(
+                    current_url,
+                    "location-header",
+                    status=expecting_location_with_status,
+                )
+            )
             next_url = urljoin(current_url, value).strip()
             current_url = next_url
             expecting_location_with_status = None
@@ -211,6 +217,46 @@ def request_with_pycurl(
 
         curl.setopt(pycurl.XFERINFOFUNCTION, progress_function)
 
+
+# TODO: body
+# TODO: decompress?
+# TODO: invalid status error (pycurl has a way I think)?
+# TODO: error serialization, error retrying conversion
+# TODO: pool of curl handles with multi (tricks from https://github.com/tornadoweb/tornado/blob/master/tornado/curl_httpclient.py)
+def request_with_pycurl(
+    url: str,
+    method: str = "GET",
+    headers: Optional[Dict[str, str]] = None,
+    follow_redirects: bool = True,
+    max_redirects: int = 5,
+    timeout: Optional[AnyTimeout] = None,
+    cancel_event: Optional[Event] = None,
+    verbose: bool = False,
+) -> PycurlResult:
+    # Preemptive cancellation
+    if cancel_event is not None and cancel_event.is_set():
+        raise CancelledRequestError
+
+    curl = pycurl.Curl()
+    buffer = BytesIO()
+    response_headers = HTTPHeaderDict()
+    stack = []
+
+    setup_curl_handle(
+        curl,
+        url=url,
+        response_headers=response_headers,
+        buffer=buffer,
+        stack=stack,
+        method=method,
+        headers=headers,
+        follow_redirects=follow_redirects,
+        max_redirects=max_redirects,
+        timeout=timeout,
+        cancel_event=cancel_event,
+        verbose=verbose,
+    )
+
     # Performing
     try:
         curl.perform()
@@ -220,17 +266,10 @@ def request_with_pycurl(
         raise coerce_error(error)
 
     status = curl.getinfo(pycurl.HTTP_CODE)
+    effective_url = curl.getinfo(pycurl.EFFECTIVE_URL)
     curl.close()
 
-    stack = []
-
-    for location, location_status in locations:
-        stack.append(Redirection(location, status=location_status))
-
-    stack.append(Redirection(current_url, status=status))
-
-    for redirection in without_last(stack):
-        redirection.type = "location-header"
+    stack.append(Redirection(effective_url, status=status))
 
     return PycurlResult(
         url=url,
