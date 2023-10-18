af499fdb4771a40e63086da4f7d85c8ffc3b72a2
Yomguithereal <guillaumeplique@gmail.com>
2023-10-18 18:18:34 +0200

    Fixing yt captions command

--- minet/youtube/scrapers.py
+++ minet/youtube/scrapers.py
@@ -17,17 +17,17 @@ from minet.web import request, create_pool_manager
 from minet.youtube.utils import ensure_video_id
 from minet.youtube.exceptions import YouTubeInvalidVideoTargetError
 
-CAPTION_TRACKS_RE = re.compile(r'({"captionTracks":.*isTranslatable":(true|false)}])')
-TIMEDTEXT_RE = re.compile(rb'timedtext?[^"]+')
+CAPTION_TRACKS_RE = re.compile(r'"captionTracks":(\[.*?\])')
 
 YOUTUBE_SCRAPER_POOL_MANAGER = create_pool_manager()
 
 YouTubeCaptionTrack = namedtuple("YouTubeCaptionTrack", ["lang", "url", "generated"])
 
 
+# Hats off to: https://github.com/algolia/youtube-captions-scraper
 def get_caption_tracks(video_id):
     # First we try to retrieve it from video info
-    url = "https://www.youtube.com/get_video_info?video_id=%s" % video_id
+    url = "https://www.youtube.com/watch?v=%s" % video_id
 
     response = request(
         url, known_encoding="utf-8", pool_manager=YOUTUBE_SCRAPER_POOL_MANAGER
@@ -38,7 +38,7 @@ def get_caption_tracks(video_id):
     m = CAPTION_TRACKS_RE.search(data)
 
     if m is not None:
-        data = json.loads(m.group(0) + "}")["captionTracks"]
+        data = json.loads("{" + m.group(0) + "}")["captionTracks"]
 
         return [
             YouTubeCaptionTrack(
@@ -46,13 +46,6 @@ def get_caption_tracks(video_id):
             )
             for item in data
         ]
-
-    # Then we try to scrape it directly from the video page
-    # url = 'https://www.youtube.com/watch?v=%s' % video_id
-
-    # response = request(url)
-
-    # timedtexts = TIMEDTEXT_RE.findall(response.data)
 
     return []
 
