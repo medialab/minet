a69002df78e51b75890e498691b2b06ad20c70d1
Yomguithereal <guillaumeplique@gmail.com>
2023-10-19 11:28:07 +0200

    Battle-testing yt captions

--- minet/cli/youtube/captions.py
+++ minet/cli/youtube/captions.py
@@ -6,6 +6,7 @@
 #
 from minet.youtube import YouTubeScraper
 from minet.youtube.types import YouTubeCaptionTrack, YouTubeCaptionLine
+from minet.youtube.exceptions import YouTubeInvalidVideoTargetError
 
 from minet.cli.utils import with_enricher_and_loading_bar
 from minet.cli.loading_bar import LoadingBar
@@ -21,11 +22,23 @@ HEADERS = YouTubeCaptionTrack.fieldnames(
 def action(cli_args, enricher, loading_bar: LoadingBar):
     scraper = YouTubeScraper()
 
-    with loading_bar.step():
-        for row, video in enricher.cells(cli_args.column, with_rows=True):
-            result = scraper.get_video_captions(video, langs=cli_args.lang)
+    lang_warning_printed = False
+
+    for row, video in enricher.cells(cli_args.column, with_rows=True):
+        with loading_bar.step():
+            try:
+                result = scraper.get_video_captions(video, langs=cli_args.lang)
+            except YouTubeInvalidVideoTargetError:
+                loading_bar.inc_stat("not-a-video", style="error")
+                continue
 
             if result is None:
+                if not lang_warning_printed:
+                    lang_warning_printed = True
+                    loading_bar.warning(
+                        "Did you forget to pass the correct value to [dim]--lang[/dim]?"
+                    )
+
                 loading_bar.inc_stat("not-found", style="error")
                 continue
 
