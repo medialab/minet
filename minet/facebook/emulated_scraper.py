d5d31ffe45734d97596c6d172fae6e9667de9c37
Yomguithereal <guillaumeplique@gmail.com>
2023-10-23 22:26:09 +0200

    Working on emulated FB scraper

--- minet/facebook/emulated_scraper.py
+++ minet/facebook/emulated_scraper.py
@@ -1,11 +1,19 @@
+from typing import Callable, Awaitable
+
+import re
 import json
-from playwright.async_api import BrowserContext, Response
+import asyncio
+from playwright.async_api import BrowserContext, Response, Page, TimeoutError
 from playwright_stealth import stealth_async
 
 from minet.browser import ThreadsafeBrowser
 
 
-def is_graphql_comments_response(response: "Response") -> bool:
+VIEW_MORE_COMMENTS_RE = re.compile(r"View\s+.+more\s+comments?", re.I)
+VIEW_MORE_REPLIES_RE = re.compile(r"(?:View\s+.+more\s+replies|\d+\s+replies)", re.I)
+
+
+def is_graphql_comments_response(response: Response) -> bool:
     if "/api/graphql/" not in response.url:
         return False
 
@@ -15,9 +23,23 @@ def is_graphql_comments_response(response: "Response") -> bool:
     )
 
 
+async def expect_comments(page: Page, fn: Callable[[], Awaitable[None]]):
+    async with page.expect_response(is_graphql_comments_response) as response_catcher:
+        try:
+            await fn()
+        except TimeoutError:
+            print("timeout")
+            return None
+
+    response = await response_catcher.value
+    data = await response.json()
+
+    return data
+
+
 class FacebookEmulatedScraper:
     def __init__(self):
-        self.browser = ThreadsafeBrowser(headless=False)
+        self.browser = ThreadsafeBrowser(headless=False, width=1024, height=768)
 
     def __enter__(self):
         return self
@@ -28,7 +50,7 @@ class FacebookEmulatedScraper:
     def __exit__(self, *args):
         self.stop()
 
-    async def __scrape_comments(self, context: "BrowserContext", url: str):
+    async def __scrape_comments(self, context: BrowserContext, url: str):
         async with await context.new_page() as page:
             await stealth_async(page)
 
@@ -36,14 +58,44 @@ class FacebookEmulatedScraper:
             await page.get_by_label("Decline optional cookies").first.click()
             await page.get_by_label("Close").first.click()
 
-            async with page.expect_response(
-                is_graphql_comments_response
-            ) as response_catcher:
+            # NOTE: this is only needed for debug
+            await page.evaluate(
+                """
+                () => {
+                    document.querySelector('[data-nosnippet]').remove();
+                }
+                """
+            )
+
+            async def select_all_comments():
                 await page.get_by_text("Most relevant").first.click()
                 await page.get_by_text("All comments").first.click()
 
-            response = await response_catcher.value
-            data = await response.json()
+            async def view_more_replies():
+                await page.get_by_text(VIEW_MORE_REPLIES_RE).first.click(timeout=3)
+
+            async def view_more_comments():
+                await page.get_by_text(VIEW_MORE_COMMENTS_RE).click(timeout=10)
+
+            data = await expect_comments(page, select_all_comments)
+
+            # while True:
+            #     await asyncio.sleep(1)
+            #     data = await expect_comments(page, view_more_replies)
+
+            #     if data is None:
+            #         break
+
+            while True:
+                # await asyncio.sleep(1)
+                data = await expect_comments(page, view_more_comments)
+
+                if data is None:
+                    break
+
+            # TODO: View x more replies, View x more comments
+
+            await asyncio.sleep(1000000)
 
             with open("./dump.json", "w") as f:
                 json.dump(data, f, ensure_ascii=False, indent=2)
