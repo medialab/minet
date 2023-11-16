ac5fcec9e8801c1f4af95e234669ae3f444418e8
Yomguithereal <guillaumeplique@gmail.com>
2023-11-16 09:48:15 +0100

    Fixing mc commands hook

--- minet/cli/mediacloud/utils.py
+++ minet/cli/mediacloud/utils.py
@@ -3,7 +3,7 @@ from minet.cli.utils import with_fatal_errors
 from minet.mediacloud.exceptions import MediacloudServerError
 
 
-def fatal_errors_hook(e):
+def fatal_errors_hook(_, e):
     if isinstance(e, MediacloudServerError):
         return ["Aborted due to a mediacloud server error:", e.server_error]
 
