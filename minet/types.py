21fcd41f943acc0c956eb30846b1a60301cd6c6b
Yomguithereal <guillaumeplique@gmail.com>
2023-12-19 11:07:58 +0100

    Adding type related conditional imports

--- minet/types.py
+++ minet/types.py
@@ -8,9 +8,9 @@ from ebbe import format_repr
 
 # NOTE: yes this is repetitive, but mypy only understands this, don't ask questions...
 if sys.version_info >= (3, 8):
-    from typing import Literal
+    from typing import Literal, get_origin, get_type_hints, get_args
 else:
-    from typing_extensions import Literal
+    from typing_extensions import Literal, get_origin, get_type_hints, get_args
 
 if sys.version_info >= (3, 10):
     from typing import TypeGuard, Concatenate, ParamSpec
