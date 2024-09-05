132ab429981564c53ec8aba471cfde8fb82438cf
Yomguithereal <guillaumeplique@gmail.com>
2024-09-05 15:06:40 +0200

    Forwarding SelectionError from minet.scrape Fix #983

--- minet/scrape/__init__.py
+++ minet/scrape/__init__.py
@@ -5,7 +5,7 @@
 # Module exposing utilities related to minet's scraping DSL.
 #
 from minet.scrape.classes.definition import scrape, DefinitionScraper, validate
-from minet.scrape.soup import WonderfulSoup
+from minet.scrape.soup import WonderfulSoup, SelectionError
 from minet.scrape.regex import (
     extract_encodings_from_xml,
     extract_canonical_link,
@@ -18,6 +18,7 @@ __all__ = [
     "DefinitionScraper",
     "validate",
     "WonderfulSoup",
+    "SelectionError",
     "extract_encodings_from_xml",
     "extract_canonical_link",
     "extract_javascript_relocation",
