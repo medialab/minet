35ee68927ca8a1ddbf17b25bdd845378895bb25e
Yomguithereal <guillaumeplique@gmail.com>
2025-02-19 16:05:43 +0100

    Dropping now useless ftest

--- ftest/facebook_user_infos.py
+++
@@ -1,9 +0,0 @@
-from minet.facebook import FacebookMobileScraper
-from minet.cli.console import console
-
-scraper = FacebookMobileScraper(cookie="firefox")
-
-USERS_URL = ["https://www.facebook.com/guillaume.plique.9/"]
-
-for url in USERS_URL:
-    console.print(scraper.user_infos(url), highlight=True)
