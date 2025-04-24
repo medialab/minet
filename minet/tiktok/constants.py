84a0aaec34b14a3d6aaa8cad044728a98a16df68
bmaz <beatrice.mazoyer@gmail.com>
2025-04-24 12:12:02 +0200

    Creating TiktokCommercialContent dataclass

--- minet/tiktok/constants.py
+++ minet/tiktok/constants.py
@@ -13,5 +13,7 @@ TIKTOK_MIN_TIME_RETRYER = 5.0
 TIKTOK_MAX_RANDOM_ADDENDUM = 1.0
 
 TIKTOK_HTTP_API_BASE_URL = "https://open.tiktokapis.com/v2"
-TIKTOK_COMMERCIAL_CONTENTS_FIELDS = "id,create_timestamp,brand_names,creator,videos"
+TIKTOK_COMMERCIAL_CONTENTS_FIELDS = (
+    "id,create_timestamp,create_date,label,brand_names,creator,videos"
+)
 TIKTOK_COMMERCIAL_CONTENTS_MAX_COUNT = 50
