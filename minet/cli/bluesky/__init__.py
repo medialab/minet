eba724a600723c39cbd0a0cfc966da4159e3360c
echevale <evan.chevalerias@gmail.com>
2025-09-10 11:14:59 +0200

    adding 9 new flags to the search-posts function

--- minet/cli/bluesky/__init__.py
+++ minet/cli/bluesky/__init__.py
@@ -13,6 +13,60 @@ BLUESKY_HTTP_API_COMMON_ARGUMENTS = [
         "rc_key": ["bluesky", "password"],
         "action": ConfigAction,
     },
+    {
+        "flags": ["-l", "--lang"],
+        "help": 'Bluesky Filter to posts in the given language. Expected to be based on post language field, though server may override language detection. Equivalent to "lang:<lang>" in classic search syntax.',
+        "rc_key": ["bluesky", "lang"],
+        "action": ConfigAction,
+    },
+    {
+        "flag": "--since",
+        "help": "Bluesky Filter results for posts after the indicated datetime (inclusive). Expected to use 'sortAt' timestamp, which is the minimum of 'createdAt' and 'indexedAt'. Can be a datetime, or just an ISO date (yyyy-mm-dd, yyyy-mm-ddThh:mm:ssZ or yyyy-mm-ddThh:mm:ss.msZ). Equivalent to \"since:<date>\" in classic search syntax.",
+        "rc_key": ["bluesky", "since"],
+        "action": ConfigAction,
+    },
+    {
+        "flag": "--until",
+        "help": "Bluesky Filter results for posts before the indicated datetime (NOT inclusive). Expected to use 'sortAt' timestamp, which is the minimum of 'createdAt' and 'indexedAt'. Can be a datetime, or just an ISO date (yyyy-mm-dd, yyyy-mm-ddThh:mm:ssZ or yyyy-mm-ddThh:mm:ss.msZ). Equivalent to \"until:<date>\" in classic search syntax.",
+        "rc_key": ["bluesky", "until"],
+        "action": ConfigAction,
+    },
+    {
+        "flag": "--mentions",
+        "help": 'Bluesky Filter to posts which mention the given account (with or without the @). Handles are resolved to DID before query-time. Only matches rich-text facet mentions. Equivalent to "mentions:<account>" in classic search syntax.',
+        "rc_key": ["bluesky", "mentions"],
+        "action": ConfigAction,
+    },
+    {
+        "flag": "--author",
+        "help": 'Bluesky Filter to posts by the given account (with or without the @). Handles are resolved to DID before query-time. Equivalent to "from:<account>" in classic search syntax.',
+        "rc_key": ["bluesky", "author"],
+        "action": ConfigAction,
+    },
+    {
+        "flag": "--domain",
+        "help": 'Bluesky Filter to posts with URLs (facet links or embeds) linking to the given domain (hostname). Server may apply hostname normalization. Equivalent to "domain:<domain>" in classic search syntax.',
+        "rc_key": ["bluesky", "domain"],
+        "action": ConfigAction,
+    },
+    {
+        "flag": "--url",
+        "help": "Bluesky Filter to posts with links (facet links or embeds) pointing to this URL. Server may apply URL normalization or fuzzy matching. The only equivalent in classic search syntax could be typing the URL as a keyword.",
+        "rc_key": ["bluesky", "url"],
+        "action": ConfigAction,
+    },
+    {
+        "flags": ["--tag", "--hashtag"],
+        "help": 'Filter to posts with the given tag (hashtag). You can include the hash (#) prefix if you wish. Multiple tags can be specified (with "AND" matching), writing them between quotes and separating them by a space. Equivalent to "#<tag>" in classic search syntax.',
+        "rc_key": ["bluesky", "tag"],
+        "action": ConfigAction,
+    },
+    {
+        "flags": ["--not-keywords", "--not"],
+        "help": 'Filter posts without the given keyword. Multiple keywords can be specified (with "AND" matching), writing them between quotes and separating them by a space. Equivalent to "-<keyword>" in classic search syntax.',
+        "rc_key": ["bluesky", "not_keywords"],
+        "action": ConfigAction,
+    },
 ]
 
 BLUESKY_FIREHOSE_COMMAND = command(
--- minet/cli/bluesky/search_posts.py
+++ minet/cli/bluesky/search_posts.py
@@ -10,6 +10,8 @@ from minet.cli.bluesky.utils import with_bluesky_fatal_errors
 
 from minet.bluesky import BlueskyHTTPClient
 
+import re
+
 
 @with_bluesky_fatal_errors
 @with_enricher_and_loading_bar(
@@ -22,9 +24,44 @@ from minet.bluesky import BlueskyHTTPClient
 def action(cli_args, enricher: Enricher, loading_bar: LoadingBar):
     client = BlueskyHTTPClient(cli_args.identifier, cli_args.password)
 
+    flags_str = ""
+    if cli_args.lang:
+        flags_str += f"\nlang:{cli_args.lang}"
+    if cli_args.since:
+        flags_str += f"\nsince:{cli_args.since}"
+    if cli_args.until:
+        flags_str += f"\nuntil:{cli_args.until}"
+    if cli_args.mentions:
+        cli_args.mentions = re.sub(r"^@?", "", cli_args.mentions)
+        flags_str += f"\nmentions:{cli_args.mentions}"
+    if cli_args.author:
+        cli_args.author = re.sub(r"^@?", "", cli_args.author)
+        flags_str += f"\nauthor:{cli_args.author}"
+    if cli_args.domain:
+        flags_str += f"\ndomain:{cli_args.domain}"
+    if cli_args.url:
+        flags_str += f"\nurl:{cli_args.url}"
+    if cli_args.tag:
+        cli_args.tag = [re.sub(r"^#?", "", tag) for tag in cli_args.tag.split(" ")]
+        flags_str += f"\ntag:{cli_args.tag}"
+    if cli_args.not_keywords:
+        cli_args.not_keywords = cli_args.not_keywords.split(" ")
+        flags_str += f"\nnot:{cli_args.not_keywords}"
+
     for row, query in enricher.cells(cli_args.column, with_rows=True):
-        with loading_bar.step(query):
-            for post in client.search_posts(query):
+        with loading_bar.step(f"{query}{flags_str}"):
+            for post in client.search_posts(
+                query,
+                lang=cli_args.lang,
+                since=cli_args.since,
+                until=cli_args.until,
+                mentions=cli_args.mentions,
+                author=cli_args.author,
+                domain=cli_args.domain,
+                url=cli_args.url,
+                tag=cli_args.tag,
+                not_keywords=cli_args.not_keywords,
+            ):
                 post_row = format_post_as_csv_row(post)
                 enricher.writerow(row, post_row)
                 loading_bar.nested_advance()
