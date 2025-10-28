2fc47bc2a29251a890001e9a7d678da54e1028d5
Yomguithereal <guillaumeplique@gmail.com>
2025-10-28 10:47:47 +0100

    Drafting TwitterUnauthenticatedAPIScraper

--- ftest/bluesky.py
+++ ftest/bluesky.py
@@ -38,24 +38,24 @@ client = BlueskyHTTPClient(conf["bluesky"]["identifier"], conf["bluesky"]["passw
 # print(client.refresh_session())
 
 post_urls = [
-#     "https://bsky.app/profile/pecqueuxanthony.bsky.social/post/3lkizm6uvhc2b",
-#     "https://bsky.app/profile/annemascret.bsky.social/post/3lkksxdlabk2y",
-#     "https://bsky.app/profile/pierre.senellart.com/post/3lkhaibfxv22b",
-#     "https://bsky.app/profile/servannemarzin.bsky.social/post/3lkgoqgzhg22k",
-#     "https://bsky.app/profile/roppick.bsky.social/post/3lk7najt7s22j",
-#     "https://bsky.app/profile/tracklist.com.br/post/3lk7xgxguf22k",
-#     "https://bsky.app/profile/snesupfsu.bsky.social/post/3lkkbdz7uxs2s",
-#     "https://bsky.app/profile/p4bl0.net/post/3lkgnjzh6ec2g",
-#     "https://bsky.app/profile/p4bl0.net/post/3lkeafh3kt22v",
-#     "https://bsky.app/profile/bricabraque.bsky.social/post/3lkdnb2gtzk2c",
-#     "https://bsky.app/profile/alinenaft.bsky.social/post/3kcoegjbnp525",
-#     "https://bsky.app/profile/zahiahamdane.bsky.social/post/3lk4jgruhzk2s",
-#     "https://bsky.app/profile/boogheta.bsky.social/post/3ljpthyfcqs2e",
-#     "https://bsky.app/profile/shiseptiana.bsky.social/post/3lkbalaxeys2v",
-#     "https://bsky.app/profile/danabra.mov/post/3ll4whwghy22y",
-#     "https://bsky.app/profile/yomguithereal.bsky.social/post/3kbg7jgtddn2w",
-#     "https://bsky.app/profile/fredbourget.bsky.social/post/3lpoug3ljdw27"
-      "https://bsky.app/profile/NewYork.activitypub.awakari.com.ap.brid.gy/post/3lkctsyce5f42"
+    #     "https://bsky.app/profile/pecqueuxanthony.bsky.social/post/3lkizm6uvhc2b",
+    #     "https://bsky.app/profile/annemascret.bsky.social/post/3lkksxdlabk2y",
+    #     "https://bsky.app/profile/pierre.senellart.com/post/3lkhaibfxv22b",
+    #     "https://bsky.app/profile/servannemarzin.bsky.social/post/3lkgoqgzhg22k",
+    #     "https://bsky.app/profile/roppick.bsky.social/post/3lk7najt7s22j",
+    #     "https://bsky.app/profile/tracklist.com.br/post/3lk7xgxguf22k",
+    #     "https://bsky.app/profile/snesupfsu.bsky.social/post/3lkkbdz7uxs2s",
+    #     "https://bsky.app/profile/p4bl0.net/post/3lkgnjzh6ec2g",
+    #     "https://bsky.app/profile/p4bl0.net/post/3lkeafh3kt22v",
+    #     "https://bsky.app/profile/bricabraque.bsky.social/post/3lkdnb2gtzk2c",
+    #     "https://bsky.app/profile/alinenaft.bsky.social/post/3kcoegjbnp525",
+    #     "https://bsky.app/profile/zahiahamdane.bsky.social/post/3lk4jgruhzk2s",
+    #     "https://bsky.app/profile/boogheta.bsky.social/post/3ljpthyfcqs2e",
+    #     "https://bsky.app/profile/shiseptiana.bsky.social/post/3lkbalaxeys2v",
+    #     "https://bsky.app/profile/danabra.mov/post/3ll4whwghy22y",
+    #     "https://bsky.app/profile/yomguithereal.bsky.social/post/3kbg7jgtddn2w",
+    #     "https://bsky.app/profile/fredbourget.bsky.social/post/3lpoug3ljdw27"
+    "https://bsky.app/profile/NewYork.activitypub.awakari.com.ap.brid.gy/post/3lkctsyce5f42"
 ]
 
 did_at_uris = [client.resolve_post_url(post_url) for post_url in post_urls]
@@ -66,14 +66,14 @@ for post_data in client.get_posts(did_at_uris, return_raw=True):
     console.print(post_data, highlight=True)
     DATA.append(post_data)
 
-#profiles = [
+# profiles = [
 #    "yomguithereal.bsky.social",
 #    "boogheta.bsky.social",
 #    "medialab-scpo.bsky.social",
 #    "nytimes.com",
-#]
+# ]
 
-#for profile in client.get_profiles(profiles):
+# for profile in client.get_profiles(profiles):
 #    DATA.append(profile)
 #    console.print(profile, highlight=True)
 
--- minet/twitter/__init__.py
+++ minet/twitter/__init__.py
@@ -1,4 +1,13 @@
 from minet.twitter.api_client import TwitterAPIClient
-from minet.twitter.api_scraper import TwitterAPIScraper, TwitterGuestAPIScraper
+from minet.twitter.api_scraper import (
+    TwitterAPIScraper,
+    TwitterGuestAPIScraper,
+    TwitterUnauthenticatedAPIScraper,
+)
 
-__all__ = ["TwitterAPIClient", "TwitterAPIScraper", "TwitterGuestAPIScraper"]
+__all__ = [
+    "TwitterAPIClient",
+    "TwitterAPIScraper",
+    "TwitterGuestAPIScraper",
+    "TwitterUnauthenticatedAPIScraper",
+]
