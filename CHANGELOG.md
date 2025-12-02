fa4280bff8e403a3aeec2a1539798d280394a800
Yomguithereal <guillaumeplique@gmail.com>
2025-12-02 11:16:17 +0100

    Bump 4.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## 4.2.1
+
+*Fixes*
+
+- Fixing CI
+
 ## 4.2.0
 
 *Features*
--- minet/__version__.py
+++ minet/__version__.py
@@ -1,3 +1,3 @@
-__version__ = "4.2.0"
+__version__ = "4.2.1"
 __published__ = "2025-12-02"
 __identifier__ = "%s (%s)" % (__version__, __published__)
